b2cacc0f5b89cbfa71e65dc49a326d790834fe32
Ankit <ankit.kumar1@tokopedia.com>
2022-05-06 16:40:39 +0530

    package structuring, remote call method added

---
+++ app/src/main/java/lostankit7/droid/stockmarket/data/remote/StockApi.kt
@@ -0,0 +1,19 @@
+package lostankit7.droid.stockmarket.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface StockApi {
+
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getListings(
+        @Query("apikey") apiKey: String
+    ) : ResponseBody
+
+    companion object {
+        const val API_KEY = "PP3Z1V631OHVA4ST"
+        const val BASE_URL = "https://alphavantage.co"
+    }
+}
